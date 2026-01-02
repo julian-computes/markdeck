7c61a8f516e562690c165c32e403fdbb3b345567
Julian Fernandez <jufer002.work@gmail.com>
2026-01-02 18:55:07 -0500

    sketch slide parsing

---
+++ README.md
@@ -0,0 +1,7 @@
+# markdeck
+
+View markdown content as a slide deck in your terminal.
+
+## How it works
+
+`H1` and `H2` elements mark the beginning of a new slide.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use markdown::{ParseOptions, mdast::Node, to_mdast};
+
 fn main() {
-    println!("Hello, world!");
+    let readme = std::fs::read_to_string("README.md").unwrap();
+    let mut mdast = to_mdast(readme.as_str(), &ParseOptions::default()).unwrap();
+
+    let mut current_slide_content = vec![];
+    let mut slides = vec![];
+    let children = mdast.children_mut().unwrap();
+
+    for node in children {
+        if !current_slide_content.is_empty()
+            && let Node::Heading(_) = node
+        {
+            // Move the current slide into the slides list
+            slides.push(std::mem::take(&mut current_slide_content));
+        }
+
+        current_slide_content.push(node);
+    }
+
+    // Push the last slide
+    slides.push(current_slide_content);
+    dbg!(slides);
 }
